bef6259f717e543e14729a59c373e3ecb9550e1d
badiuzzamanbabar <badiuzzamanbabar@yahoo.com>
2018-01-26 23:01:46 +0500

    Android projec BCSF14M523

---
+++ MCProject/app/src/main/java/com/homework/osama/mcproject/message.java
@@ -0,0 +1,6 @@
+package com.homework.osama.mcproject;
+
+public class message {
+    public String msg;
+
+}
---
+++ MCProject/app/src/main/java/com/homework/osama/mcproject/user.java
@@ -0,0 +1,7 @@
+package com.homework.osama.mcproject;
+
+
+public class user {
+    public String uname;
+   
+}
